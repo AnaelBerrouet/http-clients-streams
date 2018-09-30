88320137c48ee1427c2583a0e9f387978e2d3bfd
AnaelBerrouet <anael.jberrouet@gmail.com>
2018-09-30 23:21:17 +0000

    added response to callback

--- https-example.js
+++ https-example.js
@@ -1,1 +1,18 @@
-var http = require('http');
+var https = require('https');
+
+var options = {
+  host: 'www.wikipedia.org',
+  path: '/'
+};
+
+// called by https when the request is made.
+var callback = function(response) {
+  console.log('In response handler callback!');
+  console.log("response:", response);
+};
+
+console.log("I'm about to make the request!");
+
+https.request(options, callback).end();
+
+console.log("I've made the request!");
